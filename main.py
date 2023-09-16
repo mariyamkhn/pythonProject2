d137b823434bc96123265cc950eb528c6f2855a0
mariyamkhn <mariyamkhn85@gmail.com>
2023-09-16 15:54:54 +0530

    concatenation

---
+++ main.py
@@ -0,0 +1,10 @@
+#concatenation
+fruits = ['apple','mango','litchi','orange','kiwi','papaya','melon']
+vegetables = ['brinjal','lemon','jackfruit','tomato','chilli','potato','ginger']
+print('i love eating'+' ' + fruits[0] +' ' +'and' + " "+vegetables[0])
+print('i love eating'+' ' + fruits[1] +' ' +'and' + " "+vegetables[1])
+print('i love eating'+' ' + fruits[2] +' ' +'and' + " "+vegetables[2])
+print('i love eating'+' ' + fruits[3] +' ' +'and' + " "+vegetables[3])
+print('i love eating'+' ' + fruits[4] +' ' +'and' + " "+vegetables[4])
+print('i love eating'+' ' + fruits[5] +' ' +'and' + " "+vegetables[5])
+print('i love eating'+' ' + fruits[6] +' ' +'and' + " "+vegetables[6])
